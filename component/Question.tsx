56e310c60319b014ede9d6bffa694b12d9cd1fd1
hey-sbr <priyanshu.sabaar@crowdapps.in>
2026-04-30 19:00:57 +0530

    fix: update Question component structure and styling

--- component/Question.tsx
+++ component/Question.tsx
@@ -9,18 +9,18 @@ type QuestionProps = {
 
 export function Question({ question, revealAns, revealQuestion }: QuestionProps) {
   return (
-    <>
+    <div className="mt-20 ">
       {/* Question for you:  */}
 
-      <h1 className="text-3xl font-bold w-full">
+      {/* <h1 className="text-3xl font-bold w-full">
         For: <span className="text-black">{question.Person}</span>
-        </h1>
+        </h1> */}
 
       {/* Question */}
-      <h1 className="text-5xl font-bold">{`${revealQuestion ? gibrish(question.Answer) : "Click 'Reveal Question' to reveal"}`}</h1>
+      <h1 className="text-5xl font-bold my-5">{`${revealQuestion ? gibrish(question.Answer) : "Click 'Reveal Question' to reveal"}`}</h1>
 
       {/* Answer */}
       <h1 className="text-5xl font-bold text-green-600">{`${revealAns ? question.Answer : ""}`}</h1>
-    </>
+    </div>
   );
 }
